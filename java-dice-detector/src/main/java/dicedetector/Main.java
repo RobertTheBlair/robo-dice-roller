0ab66489c26d922c4eeaf0414bdf102f8342a3c6
Rob Blair <roblair@paypal.com>
2022-12-28 23:57:27 -0800

    [main] add a ui

--- java-dice-detector/src/main/java/dicedetector/Main.java
+++ java-dice-detector/src/main/java/dicedetector/Main.java
@@ -1,7 +1,27 @@
 package dicedetector;
 
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    JMenu menu;
+    JMenuItem a1,a2;
+    public Main() {
+        JFrame a = new JFrame("Hello world!");
+        menu = new JMenu("options");
+        JMenuBar m1 = new JMenuBar();
+        a1 = new JMenuItem("item 1");
+        a2 = new JMenuItem("item 2");
+        menu.add(a1);
+        menu.add(a2);
+        m1.add(menu);
+        a.setJMenuBar(m1);
+        a.setSize(400,400);
+        a.setLayout(null);
+        a.setVisible(true);
     }
+    public static void main(String[] args) {
+        new Main();
+    }
+
 }
